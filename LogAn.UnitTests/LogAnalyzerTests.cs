6ca42acb9f6ece167befa6a11ebb284483b81113
Sabbir Ahmed Shubo <sabbir.ove@gmail.com>
2021-10-05 17:23:14 +0600

    Add project files.

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+
+
+        [TestCase("Filename.slf",true)]
+        [TestCase("Filename.SLF",true)]
+        [TestCase("Filename.SLF",false)]
+        public void IsalidLogFileName_ValidExtensions_ReturnsTrue(string file, bool expected)
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+            bool result = analyzer.IsValidLogFileName(file);
+
+            Assert.AreEqual(result,expected);
+        }
+    }
+}
